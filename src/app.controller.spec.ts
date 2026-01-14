1c0cea4c7def6771f97e0bb5c0c56a40f4abf27f
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2026-01-14 15:10:52 -0300

    refactor(tests): integrate ConfigModule for test environment setup

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,3 +1,4 @@
+import { ConfigModule } from '@nestjs/config';
 import { Test, type TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,6 +8,17 @@ describe('AppController', () => {
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: ['.env.test'],
+          validate: () => ({
+            NODE_ENV: 'test',
+            PORT: 3000,
+            DATABASE_URL: 'postgresql://test:test@localhost:5432/test_db?schema=public',
+          }),
+        }),
+      ],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
